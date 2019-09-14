6ff5e17bdb94cc2d203e527dfbb5ed16a57afa7f
Denis <denis.rezni@gmail.com>
2019-09-14 13:06:39 +0300

    Initial commit

---
+++ app/src/main/java/com/example/calculatorapp/MainActivity.kt
@@ -0,0 +1,106 @@
+package com.example.calculatorapp
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.math.min
+
+class MainActivity : AppCompatActivity() {
+    lateinit var buttons: Array<Button>
+    val operations: Set<Char> = setOf('+', '-', '*', '/')
+
+    enum class State {
+        START, FIRST_OPERAND, SIGN, SECOND_OPERAND, RESULT
+    }
+
+    var state = State.START;
+
+    val lengthBound = 15
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        buttons = arrayOf(
+            buttonNumber0, buttonNumber1, buttonNumber2,
+            buttonNumber3, buttonNumber4, buttonNumber5, buttonNumber6,
+            buttonNumber7, buttonNumber8, buttonNumber9, buttonReset,
+            buttonPlus, buttonMinus, buttonDivide, buttonEquals, buttonMultiply
+        )
+        buttons.forEach { b ->
+            b.setOnClickListener { _ ->
+                updateLabel(b.text[0])
+            }
+        }
+    }
+
+    fun getResult(): String {
+        //parsing the expression
+        val text = calcResult.text.replace(Regex(" "), "")
+        var pointer = 0
+        val firstOperand = getNumberString(pointer, text)
+        pointer += firstOperand.length
+        if (pointer >= text.length) {
+            return firstOperand
+        }
+        val sign = text[pointer]
+        pointer++
+        val secondOperand = getNumberString(pointer, text)
+//        return firstOperand + " " + secondOperand
+        return evaluate(firstOperand.toInt().toDouble(), secondOperand.toInt().toDouble(), sign).toString()
+    }
+
+    fun getNumberString(pos: Int, text: String): String {
+        //searching for an integer
+        var pointer = pos
+        while (pointer < text.length && text[pointer].isDigit()) {
+            pointer++
+        }
+        return text.substring(pos, pointer)
+    }
+
+    fun evaluate(firstOperand: Double, secondOperand: Double, operation: Char): Double {
+        when (operation) {
+            '+' -> return firstOperand + secondOperand
+            '-' -> return firstOperand - secondOperand
+            '*' -> return firstOperand * secondOperand
+            '/' -> return firstOperand / secondOperand
+        }
+        return 0.0
+    }
+
+    @SuppressLint("SetTextI18n")
+    fun updateLabel(symbol: Char) {
+        if (symbol.isDigit()) {
+            if (state == State.START || state == State.RESULT) {
+                calcResult.setText(symbol.toString())
+                if(symbol != '0'){
+                    state = State.FIRST_OPERAND
+                }
+            } else if (calcResult.text.length < lengthBound) {
+                calcResult.append(symbol.toString())
+                if (state == State.SIGN) {
+                    state = State.SECOND_OPERAND
+                }
+            }
+        } else if (symbol == 'C') {
+            calcResult.setText("0")
+            state = State.START
+        } else if (operations.contains(symbol)) {
+            if (state == State.SIGN) {
+                calcResult.text = getNumberString(0, calcResult.text.toString()) + " " +
+                        symbol + " "
+            } else if (state == State.FIRST_OPERAND) {
+                calcResult.append(" $symbol ")
+                state = State.SIGN
+            }
+        } else if (symbol == '=') {
+            val result = getResult()
+            calcResult.setText(result.substring(0, min(lengthBound, result.length)))
+            state = State.RESULT
+        }
+
+    }
+
+}
